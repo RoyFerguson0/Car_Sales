e228ebdcedae0a3c23763b1232cd9fd728fbf710
mkinkead <mkinkead@M2L110TC01.belfastmet.int>
2023-04-21 09:45:40 +0100

    Martin has added a test class - 21/04/2023 @ 09:45

---
+++ src/oop_template/Test.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop_template;
+
+/**
+ *
+ * @author mkinkead
+ */
+public class Test {
+    
+    // Martin was here!
+    
+}
