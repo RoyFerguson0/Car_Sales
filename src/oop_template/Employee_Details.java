87879857e481c008c85293fbf6e447409ae735d4
50053641 <50053641@M2L201AC07.belfastmet.int>
2023-05-04 15:37:17 +0100

    15:26 // JavaDoc // Jack Holmes

--- src/oop_template/Employee_Details.java
+++ src/oop_template/Employee_Details.java
@@ -8,6 +8,10 @@ package oop_template;
  *
  * @author Jackh
  */
+/**
+     * Creating public class Employee Details
+     * @author Jack Holmes (50053641)
+     */
 public class Employee_Details {
     
     private int employeeID;
@@ -23,7 +27,10 @@ public class Employee_Details {
  public Employee_Details(){
 }
 
-
+/**
+     * Overwriting toString to ensure that it is viewed in the list correct 
+     * @author Jack Holmes (50053641)
+     */
 
 //Setting Objects
  @Override
@@ -32,41 +39,81 @@ public String toString(){
         return title + " " + forename + " " + surname;
     }
         
+/**
+     * Setting the Employee ID - Integer 
+     * @param employeeID int
+     * @author Jack Holmes (50053641)
+     */
 //Employee ID      
 public void setEmployeeID(int employeeID) {
     this.employeeID = employeeID;
 }
 
+/**
+     * Setting the Employee Title - String
+     * @param title String
+     * @author Jack Holmes (50053641)
+     */
 //Title
 public void setTitle(String title){
     this.title = title;
 }
 
+/**
+     * Setting the Employee Forename - String
+     * @param forename String
+     * @author Jack Holmes (50053641)
+     */
 //Forename
 public void setForename(String forename){
     this.forename = forename;
 }
 
+/**
+     * Setting the Employee Surname - String
+     * @param surname String
+     * @author Jack Holmes (50053641)
+     */
 //Surname
 public void setSurname(String surname){
     this.surname = surname;
 }
 
+/**
+     * Setting the Employee Gender - String
+     * @param gender String
+     * @author Jack Holmes (50053641)
+     */
 //Gender
 public void setGender(String gender){
     this.gender = gender;
 }
 
+/**
+     * Setting the Employee Job Title - String
+     * @param job_title String
+     * @author Jack Holmes (50053641)
+     */
 //Job Title
 public void setJobTitle(String job_title){
     this.job_title = job_title;
 }
 
+/**
+     * Setting the Employee Forename - String
+     * @param contracted_hours Int
+     * @author Jack Holmes (50053641)
+     */
 //Contracted hours
 public void setContractedHours(int contracted_hours){
     this.contracted_hours = contracted_hours;
 }
 
+/**
+     * Setting the Employee Forename - String
+     * @param hourly_rate int
+     * @author Jack Holmes (50053641)
+     */
 //Hourly Rate
 public void setHourlyRate(int hourly_rate){
     this.hourly_rate = hourly_rate;
@@ -78,41 +125,81 @@ public void setHourlyRate(int hourly_rate){
 
 //Getters
 
+/**
+     * Getting the EmployeeID - int
+     * @return employeeID Int
+     * @author Jack Holmes (50053641)
+     */
 //EmployeeID
 public int getEmployeeID() {
     return employeeID;
 }
 
+/**
+     * Getting the Employee Title - String
+     * @return title String
+     * @author Jack Holmes (50053641)
+     */
 //Title
 public String getTitle() {
     return title;
 }
 
+/**
+     * Getting the Employee Forename - String
+     * @return forename String
+     * @author Jack Holmes (50053641)
+     */
 //Forename
 public String getForename() {
     return forename;
 }
 
+/**
+     * Getting the Employee Surname - String
+     * @return surname String
+     * @author Jack Holmes (50053641)
+     */
 //Surname
 public String getSurname() {
     return surname;
 }
 
+/**
+     * Getting the Employee Gender - String
+     * @return gender String
+     * @author Jack Holmes (50053641)
+     */
 //Gender
 public String getGender() {
     return gender;
 }
 
+/**
+     * Getting the Employee Job Title - String
+     * @return job_title String
+     * @author Jack Holmes (50053641)
+     */
 //Job Title
 public String getJobTitle() {
     return job_title;
 }
 
+/**
+     * Getting the Employee Contracted Hours - int
+     * @return contracted_hours int
+     * @author Jack Holmes (50053641)
+     */
 //Contracted Hours
 public int getContractedHours() {
     return contracted_hours;
 }
 
+/**
+     * Getting the Employee Hourly Rate - int
+     * @return hourly_rate int
+     * @author Jack Holmes (50053641)
+     */
 //Hourly Rate
 public int getHourlyRate() {
     return hourly_rate;
